f34497e4cfeefdcf050d342605662d7842d8745d
mo523 <43011781+mo523@users.noreply.github.com>
2018-10-16 15:19:04 -0400

    Organized and removed files

--- src/ChessDriver.java
+++ src/ChessDriver.java
@@ -5,9 +5,7 @@ import static org.fusesource.jansi.Ansi.Color.*;
 
 public class ChessDriver
 {
-
 	static Scanner kyb = new Scanner(System.in);
-	static final int Max_Num_Of_Pieces = 16;
 
 	public static void main( String[] args )
 	{
@@ -56,8 +54,7 @@ public class ChessDriver
 		}
 
 		display(CB);
-		// boolean white = true;
-		// System.out.println("Player 1 (white), what is your name?");
+		System.out.println("Player 1 (white), what is your name?");
 		// Player p1 = new Player(white, kyb.next());
 		// System.out.println("Player 2 (black), what is your name?");
 		// Player p2 = new Player(!white, kyb.next());
@@ -65,146 +62,88 @@ public class ChessDriver
 		// playGame(p1, p2);
 	}
 
-	/*public static void playGame( Player p1, Player p2 )
-	{
-
-		do
-		{
-			if ( p1.isNotInCheckMate() )
-				eachPlayersMove(p1, p2);
-			if ( p2.isNotInCheckMate() )
-				eachPlayersMove(p2, p1);
-		} while ( p1.isNotInCheckMate() && p2.isNotInCheckMate() );
-	}
-
-	public static void eachPlayersMove( Player movingPlayer, Player passivePlayer )
-	{
-		boolean legalMove = true;
-		String from = null;
-		String to = null;
-
-		displayBoard();
-
-		do
-		{
-			if ( !legalMove )
-				System.out.println("Illegal complete move, try again");
-			System.out.println(movingPlayer.getName() + ", what is your moves first position?");
-			from = getMoveValuesAndTestThem(from);
-			System.out.println(movingPlayer.getName() + ", what is your moves second position?");
-			to = getMoveValuesAndTestThem(to);
-			legalMove = movingPlayer.movePiece(from, to);
-			if ( legalMove )
-				killPiece(passivePlayer, to);
-		} while ( !legalMove );
-	}
-
-	public static String getMoveValuesAndTestThem( String moveValue )
-	{
-		boolean goodMove = true;
-		do
-		{
-			if ( !goodMove )
-				System.out.println("Illegal individual position, try again");
-			moveValue = convertPositionToNumbers(kyb.next());
-			goodMove = ( moveValue.charAt(0) >= 48 && moveValue.charAt(0) <= 55 && moveValue.charAt(1) >= 48
-					&& moveValue.charAt(1) <= 55 );
-		} while ( !goodMove );
-		return moveValue;
-	}
-
-	public static void killPiece( Player p, String to )
-	{
-		for ( int i = 0; i < Max_Num_Of_Pieces; i++ )
-		{
-			if ( p.getPieces()[i].getPosition().equalsIgnoreCase(to) )
-			{
-				p.getPieces()[i] = null;
-				break;
-			}
-		}
-	}
-
-	public static void displayBoard()
-	{
-
-	}
-
-	public static String convertPositionToNumbers( String move )
-	{
-		String positionInNumbers = "";
-		switch ( move.charAt(0) )
-		{
-		case 'a':
-			positionInNumbers += "0";
-			break;
-		case 'b':
-			positionInNumbers += "1";
-			break;
-		case 'c':
-			positionInNumbers += "2";
-			break;
-		case 'd':
-			positionInNumbers += "3";
-			break;
-		case 'e':
-			positionInNumbers += "4";
-			break;
-		case 'f':
-			positionInNumbers += "5";
-			break;
-		case 'g':
-			positionInNumbers += "6";
-			break;
-		case 'h':
-			positionInNumbers += "7";
-			break;
-		default:
-			positionInNumbers += "8";
-			break;
-		}
-		positionInNumbers += Integer.toString(( Integer.parseInt(move.substring(1)) - 1 ));
-		return positionInNumbers;
-	}
-*/
+	/*
+	 * public static void playGame( Player p1, Player p2 ) {
+	 * 
+	 * do { if ( p1.isNotInCheckMate() ) eachPlayersMove(p1, p2); if (
+	 * p2.isNotInCheckMate() ) eachPlayersMove(p2, p1); } while (
+	 * p1.isNotInCheckMate() && p2.isNotInCheckMate() ); }
+	 * 
+	 * public static void eachPlayersMove( Player movingPlayer, Player passivePlayer
+	 * ) { boolean legalMove = true; String from = null; String to = null;
+	 * 
+	 * displayBoard();
+	 * 
+	 * do { if ( !legalMove )
+	 * System.out.println("Illegal complete move, try again");
+	 * System.out.println(movingPlayer.getName() +
+	 * ", what is your moves first position?"); from =
+	 * getMoveValuesAndTestThem(from); System.out.println(movingPlayer.getName() +
+	 * ", what is your moves second position?"); to = getMoveValuesAndTestThem(to);
+	 * legalMove = movingPlayer.movePiece(from, to); if ( legalMove )
+	 * killPiece(passivePlayer, to); } while ( !legalMove ); }
+	 * 
+	 * public static String getMoveValuesAndTestThem( String moveValue ) { boolean
+	 * goodMove = true; do { if ( !goodMove )
+	 * System.out.println("Illegal individual position, try again"); moveValue =
+	 * convertPositionToNumbers(kyb.next()); goodMove = ( moveValue.charAt(0) >= 48
+	 * && moveValue.charAt(0) <= 55 && moveValue.charAt(1) >= 48 &&
+	 * moveValue.charAt(1) <= 55 ); } while ( !goodMove ); return moveValue; }
+	 * 
+	 * public static void killPiece( Player p, String to ) { for ( int i = 0; i <
+	 * Max_Num_Of_Pieces; i++ ) { if (
+	 * p.getPieces()[i].getPosition().equalsIgnoreCase(to) ) { p.getPieces()[i] =
+	 * null; break; } } }
+	 * 
+	 * public static void displayBoard() {
+	 * 
+	 * }
+	 * 
+	 * public static String convertPositionToNumbers( String move ) { String
+	 * positionInNumbers = ""; switch ( move.charAt(0) ) { case 'a':
+	 * positionInNumbers += "0"; break; case 'b': positionInNumbers += "1"; break;
+	 * case 'c': positionInNumbers += "2"; break; case 'd': positionInNumbers +=
+	 * "3"; break; case 'e': positionInNumbers += "4"; break; case 'f':
+	 * positionInNumbers += "5"; break; case 'g': positionInNumbers += "6"; break;
+	 * case 'h': positionInNumbers += "7"; break; default: positionInNumbers += "8";
+	 * break; } positionInNumbers += Integer.toString((
+	 * Integer.parseInt(move.substring(1)) - 1 )); return positionInNumbers; }
+	 */
 	public static void display( Piece[][] CB )
 	{
 		AnsiConsole.systemInstall();
-		
-		for (int i = 0; i < 48; i++)
+
+		for ( int i = 0; i < 48; i++ )
 		{
-			for (int j = 0; j < 8; j++)
+			for ( int j = 0; j < 8; j++ )
 			{
-				if (j%2 == 0)
-					if (i/6%2==0)
-						if (CB[i/6][j] == null)
+				if ( j % 2 == 0 )
+					if ( i / 6 % 2 == 0 )
+						if ( CB[i / 6][j] == null )
 							System.out.print(ansi().bg(YELLOW).fg(BLUE).a("              ").reset());
-						else if(CB[i/6][j].isWhite())
-							System.out.print(ansi().bg(YELLOW).fgBright(WHITE).a(PieceSection(i,j, CB)).reset());
+						else if ( CB[i / 6][j].isWhite() )
+							System.out.print(ansi().bg(YELLOW).fgBright(WHITE).a(PieceSection(i, j, CB)).reset());
 						else
-							System.out.print(ansi().bg(YELLOW).fg(BLACK).a(PieceSection(i,j, CB)).reset());
+							System.out.print(ansi().bg(YELLOW).fg(BLACK).a(PieceSection(i, j, CB)).reset());
+					else if ( CB[i / 6][j] == null )
+						System.out.print(ansi().bg(WHITE).fg(BLUE).a("              ").reset());
+					else if ( CB[i / 6][j].isWhite() )
+						System.out.print(ansi().bg(WHITE).fgBright(WHITE).a(PieceSection(i, j, CB)).reset());
 					else
-						if (CB[i/6][j] == null)
-							System.out.print(ansi().bg(WHITE).fg(BLUE).a("              ").reset());
-						else if (CB[i/6][j].isWhite())
-							System.out.print(ansi().bg(WHITE).fgBright(WHITE).a(PieceSection(i,j, CB)).reset());
-						else
-							System.out.print(ansi().bg(WHITE).fg(BLACK).a(PieceSection(i,j, CB)).reset());
+						System.out.print(ansi().bg(WHITE).fg(BLACK).a(PieceSection(i, j, CB)).reset());
+				else if ( i / 6 % 2 == 0 )
+					if ( CB[i / 6][j] == null )
+						System.out.print(ansi().bg(WHITE).fg(BLUE).a("              ").reset());
+					else if ( CB[i / 6][j].isWhite() )
+						System.out.print(ansi().bg(WHITE).fgBright(WHITE).a(PieceSection(i, j, CB)).reset());
+					else
+						System.out.print(ansi().bg(WHITE).fg(BLACK).a(PieceSection(i, j, CB)).reset());
+				else if ( CB[i / 6][j] == null )
+					System.out.print(ansi().bg(YELLOW).fg(BLUE).a("              ").reset());
+				else if ( CB[i / 6][j].isWhite() )
+					System.out.print(ansi().bg(YELLOW).fgBright(WHITE).a(PieceSection(i, j, CB)).reset());
 				else
-					if (i/6%2==0)
-						if (CB[i/6][j] == null)
-							System.out.print(ansi().bg(WHITE).fg(BLUE).a("              ").reset());
-						else if (CB[i/6][j].isWhite())
-							System.out.print(ansi().bg(WHITE).fgBright(WHITE).a(PieceSection(i,j, CB)).reset());
-						else
-							System.out.print(ansi().bg(WHITE).fg(BLACK).a(PieceSection(i,j, CB)).reset());
-					else
-						if (CB[i/6][j] == null)
-							System.out.print(ansi().bg(YELLOW).fg(BLUE).a("              ").reset());
-						else if (CB[i/6][j].isWhite())
-							System.out.print(ansi().bg(YELLOW).fgBright(WHITE).a(PieceSection(i,j, CB)).reset());
-						else
-							System.out.print(ansi().bg(YELLOW).fg(BLACK).a(PieceSection(i,j, CB)).reset());
+					System.out.print(ansi().bg(YELLOW).fg(BLACK).a(PieceSection(i, j, CB)).reset());
 
 			}
 			System.out.println();
@@ -216,12 +155,12 @@ public class ChessDriver
 	public static String PieceSection( int i, int j, Piece[][] cb )
 	{
 		String fourteen;
-		if ( i%6 == 0 || i%6 == 5 )
+		if ( i % 6 == 0 || i % 6 == 5 )
 		{
 			fourteen = "              ";
 		}
 		else
-			fourteen = cb[i/6][j].getIcon(1);
+			fourteen = cb[i / 6][j].getIcon(1);
 		return fourteen;
 	}
 }
--- src/Player.java
+++ src/Player.java
@@ -3,8 +3,6 @@ public class Player {
 
 	/*private boolean white;
 	private String name;
-	private Piece[] pieces = new Piece[16];
-	static final  int Max_Num_Of_Pieces = 16;
 	public Player(boolean white, String name) {
 		this.white = white;
 		this.name = name;
@@ -36,43 +34,7 @@ public class Player {
 	
 	
 	
-	public void setWhitePieces(){
-		pieces[0] = new King("47", white);
-		pieces[1] = new Queen("37", white);
-		pieces[2] = new Bishop("57", white);
-		pieces[3] = new Bishop("27", white);
-		pieces[4] = new Horse("67", white);
-		pieces[5] = new Horse("17", white);
-		pieces[6] = new Rook("07", white);
-		pieces[7] = new Rook("77", white);
-		pieces[8] = new WhitePawn("06", white);
-		pieces[9] = new WhitePawn("16", white);
-		pieces[10] = new WhitePawn("26", white);
-		pieces[11] = new WhitePawn("36", white);
-		pieces[12] = new WhitePawn("46", white);
-		pieces[13] = new WhitePawn("56", white);
-		pieces[14] = new WhitePawn("66", white);
-		pieces[15] = new WhitePawn("76", white);
-	}
-	
-	public void setBlackPieces(){
-		pieces[0] = new King("40", !white);
-		pieces[1] = new Queen("30", !white);
-		pieces[2] = new Bishop("50", !white);
-		pieces[3] = new Bishop("20", !white);
-		pieces[4] = new Horse("60", !white);
-		pieces[5] = new Horse("10", !white);
-		pieces[6] = new Rook("00", !white);
-		pieces[7] = new Rook("70", !white);
-		pieces[8] = new BlackPawn("01", !white);
-		pieces[9] = new BlackPawn("11", !white);
-		pieces[10] = new BlackPawn("21", !white);
-		pieces[11] = new BlackPawn("31", !white);
-		pieces[12] = new BlackPawn("41", !white);
-		pieces[13] = new BlackPawn("51", !white);
-		pieces[14] = new BlackPawn("61", !white);
-		pieces[15] = new BlackPawn("71", !white);
-	}
+
 	
 
 
