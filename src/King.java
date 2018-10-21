9f510cda4454c41d907fe6a9e2a46f4dd1e2453e
Mayer Greenberg <mgreenbe17@student.touro.edu>
2018-10-21 03:59:26 -0400

    fixed kings check method

--- src/King.java
+++ src/King.java
@@ -19,7 +19,7 @@ public class King extends Piece {
 
 	@Override
 	public boolean canPieceMoveLikeThat(int from_Y_Coordinate,int from_X_Coordinate, int to_Y_Coordinate, int to_X_Coordinate) {
-		if(Math.abs(from_Y_Coordinate) > 1 || Math.abs(from_X_Coordinate) > 1 || Math.abs(to_Y_Coordinate) > 1 || Math.abs(to_X_Coordinate) > 1)
+		if(Math.abs(from_Y_Coordinate - to_Y_Coordinate) > 1 || Math.abs(from_X_Coordinate - to_X_Coordinate) > 1)
 			return false;
 		return true;
 	}
