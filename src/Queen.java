3581a08b3177cfd3614f866b9a993ff2c8f813d2
JoelSontag <44041042+JoelSontag@users.noreply.github.com>
2018-10-22 19:26:28 -0400

    Streamlined Queen's valid and used it to create Horse valid

--- src/Queen.java
+++ src/Queen.java
@@ -1,12 +1,11 @@
 
 public class Queen extends Piece {
 	private String icon[] = {"     queen    ", "              ", "              ", "              "};
-	private boolean white;
+	
 	public Queen( boolean white) {
 		super(white);
 		this.name = "Queen";
 		displayCharacter = 'q';
-		this.white=white;
 	}
 	public String getIcon(int row)
 	{
@@ -15,16 +14,5 @@ public class Queen extends Piece {
 	public boolean isWhite()
 	{
 		return white;
-	}
-	
-	public boolean canPieceMoveLikeThat(int from_Y_Coordinate, int from_X_Coordinate, int to_Y_Coordinate, int to_X_Coordinate) 
-	{
-		Rook rook = new Rook(white);
-		King king = new King(white);
-		Bishop bishop = new Bishop(white);
-		
-		return (rook.canPieceMoveLikeThat(from_Y_Coordinate, from_X_Coordinate, to_Y_Coordinate, to_X_Coordinate)||
-				king.canPieceMoveLikeThat(from_Y_Coordinate, from_X_Coordinate, to_Y_Coordinate, to_X_Coordinate)||
-				bishop.canPieceMoveLikeThat(from_Y_Coordinate, from_X_Coordinate, to_Y_Coordinate, to_X_Coordinate));
 	}
 }
