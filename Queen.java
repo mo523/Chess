c363135ce4bd464b09e2f0d3ef3ad2eaf5ee5270
JoelSontag <44041042+JoelSontag@users.noreply.github.com>
2018-10-22 19:40:36 -0400

    Updated Queen
    (I'm still learning this so bear with me)

---
+++ Queen.java
@@ -0,0 +1,30 @@
+
+public class Queen extends Piece {
+	private String icon[] = {"     queen    ", "              ", "              ", "              "};
+	private boolean white;
+	public Queen( boolean white) {
+		super(white);
+		this.name = "Queen";
+		displayCharacter = 'q';
+		this.white=white;
+	}
+	public String getIcon(int row)
+	{
+		return icon[row];
+	}
+	public boolean isWhite()
+	{
+		return white;
+	}
+	
+	@Override
+	public boolean canPieceMoveLikeThat(int from_Y_Coordinate, int from_X_Coordinate, int to_Y_Coordinate, int to_X_Coordinate) 
+	{
+		Rook rook = new Rook(white);
+		Bishop bishop = new Bishop(white);
+		
+		
+		return (rook.canPieceMoveLikeThat(from_Y_Coordinate, from_X_Coordinate, to_Y_Coordinate, to_X_Coordinate)||
+				bishop.canPieceMoveLikeThat(from_Y_Coordinate, from_X_Coordinate, to_Y_Coordinate, to_X_Coordinate));
+	}
+}
