a589882cd1582ec542b76eda9fa86a96dc37c7be
Mayer Greenberg <mgreenbe17@student.touro.edu>
2018-10-25 19:05:13 -0400

    added possible piece spots

--- src/ChessDriver.java
+++ src/ChessDriver.java
@@ -161,6 +161,38 @@ public class ChessDriver
 		CB[7][5] = new Bishop(IS_BLACK);
 		CB[7][6] = new Horse(IS_BLACK);
 		CB[7][7] = new Rook(IS_BLACK);
+		/*		CB[0][1] = new Pawn(IS_WHITE);
+		CB[1][1] = new Pawn(IS_WHITE);
+		CB[2][1] = new Pawn(IS_WHITE);
+		CB[3][1] = new Pawn(IS_WHITE);
+		CB[4][1] = new Pawn(IS_WHITE);
+		CB[5][1] = new Pawn(IS_WHITE);
+		CB[6][1] = new Pawn(IS_WHITE);
+		CB[7][1] = new Pawn(IS_WHITE);
+		CB[0][6] = new Pawn(IS_BLACK);
+		CB[1][6] = new Pawn(IS_BLACK);
+		CB[2][6] = new Pawn(IS_BLACK);
+		CB[3][6] = new Pawn(IS_BLACK);
+		CB[4][6] = new Pawn(IS_BLACK);
+		CB[5][6] = new Pawn(IS_BLACK);
+		CB[6][6] = new Pawn(IS_BLACK);
+		CB[7][6] = new Pawn(IS_BLACK);
+		CB[0][0] = new Rook(IS_WHITE);
+		CB[1][0] = new Horse(IS_WHITE);
+		CB[2][0] = new Bishop(IS_WHITE);
+		CB[3][0] = new Queen(IS_WHITE);
+		CB[4][0] = new King(IS_WHITE);
+		CB[5][0] = new Bishop(IS_WHITE);
+		CB[6][0] = new Horse(IS_WHITE);
+		CB[7][0] = new Rook(IS_WHITE);
+		CB[0][7] = new Rook(IS_BLACK);
+		CB[1][7] = new Horse(IS_BLACK);
+		CB[2][7] = new Bishop(IS_BLACK);
+		CB[3][7] = new Queen(IS_BLACK);
+		CB[4][7] = new King(IS_BLACK);
+		CB[5][7] = new Bishop(IS_BLACK);
+		CB[6][7] = new Horse(IS_BLACK);
+		CB[7][7] = new Rook(IS_BLACK);*/
 	}
 
 	public static String PieceSection( int i, int j, Piece[][] cb )
