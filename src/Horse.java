29c0c800bc123fb13a558aec3fd3239862de1864
Joel Sontag <jsontag4@student.touro.edu>
2018-11-04 23:56:46 -0500

    Horse always returns true for noPieceInWay

--- src/Horse.java
+++ src/Horse.java
@@ -42,7 +42,7 @@ public class Horse extends Piece {
 	public boolean noPieceInTheWay(int from_X_Coordinate,
 			int from_Y_Coordinate, int to_X_Coordinate, int to_Y_Coordinate,
 			Piece[][] CB) {
-		// TODO Auto-generated method stub
+		
 		return true;
 	}
 }
