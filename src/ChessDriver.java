c20ad316dbcde3c9ed5fea38c74adb6479d07b4a
mo523 <43011781+mo523@users.noreply.github.com>
2018-11-21 23:29:46 -0500

    Sped up display immensely

--- src/ChessDriver.java
+++ src/ChessDriver.java
@@ -466,23 +466,25 @@ public class ChessDriver {
 		String fgWhite = useJansi? "\u001B[97m" : "\u001B[38;2;205;205;205m";
 		String fgBlack = useJansi? "\u001B[30m" : "\u001B[38;2;40;40;40m";
 		String fgBlue = useJansi? "\u001B[34m" : "\u001B[38;2;112;140;78m";
-		System.out.println("\n" + bxWhite + fgBlack + (whitesTurn ? letters1 : letters2) + " " + reset);
+		String board  = "";
+		board += ("\n" + bxWhite + fgBlack + (whitesTurn ? letters1 : letters2) + " " + reset + "\n");
 		for (int i = out; i != minOut; i += chg) {
 			boolean numRow = i % 6 + 1 == 3;
-			System.out.print(bxWhite + fgBlack + (numRow ? i / 6 + 1 + " " : "  ") + reset);
+			board += (bxWhite + fgBlack + (numRow ? i / 6 + 1 + " " : "  ") + reset);
 			for (int j = in; j != minIn; j += chg) {
 				Boolean isWhite = chessBoard[i / 6][j] != null ? chessBoard[i / 6][j].isWhite() : null;
 				boolean ijTheSame = i / 6 % 2 == j % 2;
 				boolean isNull = chessBoard[i / 6][j] == null;
 				boolean from = i / 6 == fr && j == fc;
 				boolean to = i / 6 == tr && j == tc;
-				System.out.print((from ? bgCyan : to ? bgGreen : ijTheSame ? bgWhite : bgBlack) + (isNull ? fgBlue : isWhite ? fgWhite : fgBlack)
+				board += ((from ? bgCyan : to ? bgGreen : ijTheSame ? bgWhite : bgBlack) + (isNull ? fgBlue : isWhite ? fgWhite : fgBlack)
 						+ PieceSection(i, j) + reset);
 			}
-			System.out.print(bxWhite + fgBlack + (numRow ? " " + (i / 6 + 1) : "  ") + reset);
-			System.out.println();
+			board += (bxWhite + fgBlack + (numRow ? " " + (i / 6 + 1) : "  ") + reset);
+			board += "\n";
 		}
-		System.out.println(bxWhite + fgBlack + " " + (whitesTurn ? letters1 : letters2) + reset);
+		board += (bxWhite + fgBlack + " " + (whitesTurn ? letters1 : letters2) + reset );
+		System.out.println(board);
 	}
 
 	public static String PieceSection(int i, int j) {
@@ -490,7 +492,7 @@ public class ChessDriver {
 		boolean empty = chessBoard[i / 6][j] == null;
 		String numLet = (char) (72 - j) + "" + (i / 6 + 1);
 		int iconRow = (whitesTurn ? 47 - i : i) % 6;
-		return (empty ? "            " : chessBoard[i / 6][j].getIcon(iconRow)) + (numRow ? numLet : "  ");
+		return (empty ? "            " : chessBoard[i / 6][j].getIcon(iconRow)) + (numRow ? ("\u001B[30m" + numLet) : "  ");
 	}
 	// public static void clear() throws InterruptedException, IOException
 	// {
