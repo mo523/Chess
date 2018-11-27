3aba350684c4156b0912d013035e7f236151406c
mhirsch6 <mhirsch6@FB-W108.academic.touro.edu>
2018-11-26 19:33:06 -0500

    new debug display

--- src/Display.java
+++ src/Display.java
@@ -1,10 +1,8 @@
 import java.io.IOException;
 
-public class Display
-{
-	public static void display( boolean whitesTurn, boolean useJansi, Piece[][] chessBoard, int fr, int fc, int tr,
-			int tc )
-	{
+public class Display {
+	public static void display(boolean whitesTurn, boolean useJansi, Piece[][] chessBoard, int fr, int fc, int tr,
+			int tc) {
 		int out = whitesTurn ? 47 : 0;
 		int in = whitesTurn ? 7 : 0;
 		int minOut = whitesTurn ? -1 : 48;
@@ -22,59 +20,62 @@ public class Display
 		String fgBlack = useJansi ? "\u001B[30m" : "\u001B[38;2;40;40;40m";
 		String fgBlue = useJansi ? "\u001B[34m" : "\u001B[38;2;112;140;78m";
 		String board = "";
-		board += ( "\n" + bxWhite + fgBlack + ( whitesTurn ? letters1 : letters2 ) + " " + reset + "\n" );
-		for ( int i = out; i != minOut; i += chg )
-		{
+		board += ("\n" + bxWhite + fgBlack + (whitesTurn ? letters1 : letters2) + " " + reset + "\n");
+		for (int i = out; i != minOut; i += chg) {
 			boolean numRow = i % 6 + 1 == 3;
-			board += ( bxWhite + fgBlack + ( numRow ? i / 6 + 1 + " " : "  " ) + reset );
-			for ( int j = in; j != minIn; j += chg )
-			{
+			board += (bxWhite + fgBlack + (numRow ? i / 6 + 1 + " " : "  ") + reset);
+			for (int j = in; j != minIn; j += chg) {
 				Boolean isWhite = chessBoard[i / 6][j] != null ? chessBoard[i / 6][j].isWhite() : null;
 				boolean ijTheSame = i / 6 % 2 == j % 2;
 				boolean isNull = chessBoard[i / 6][j] == null;
 				boolean from = i / 6 == fr && j == fc;
 				boolean to = i / 6 == tr && j == tc;
-				board += ( ( from ? bgCyan : to ? bgGreen : ijTheSame ? bgWhite : bgBlack )
-						+ ( isNull ? fgBlue : isWhite ? fgWhite : fgBlack ) + PieceSection(i, j, whitesTurn, chessBoard)
-						+ reset );
+				board += ((from ? bgCyan : to ? bgGreen : ijTheSame ? bgWhite : bgBlack)
+						+ (isNull ? fgBlue : isWhite ? fgWhite : fgBlack) + PieceSection(i, j, whitesTurn, chessBoard)
+						+ reset);
 			}
-			board += ( bxWhite + fgBlack + ( numRow ? " " + ( i / 6 + 1 ) : "  " ) + reset );
+			board += (bxWhite + fgBlack + (numRow ? " " + (i / 6 + 1) : "  ") + reset);
 			board += "\n";
 		}
-		board += ( bxWhite + fgBlack + " " + ( whitesTurn ? letters1 : letters2 ) + reset );
+		board += (bxWhite + fgBlack + " " + (whitesTurn ? letters1 : letters2) + reset);
 		System.out.println(board);
 	}
 
-	public static String PieceSection( int i, int j, boolean whitesTurn, Piece[][] chessBoard )
-	{
-		boolean numRow = i % 6 == ( whitesTurn ? 0 : 5 );
+	public static String PieceSection(int i, int j, boolean whitesTurn, Piece[][] chessBoard) {
+		boolean numRow = i % 6 == (whitesTurn ? 0 : 5);
 		boolean empty = chessBoard[i / 6][j] == null;
-		String numLet = (char) ( 72 - j ) + "" + ( i / 6 + 1 );
-		int iconRow = ( whitesTurn ? 47 - i : i ) % 6;
-		return ( empty ? "            " : chessBoard[i / 6][j].getIcon(iconRow) )
-				+ ( numRow ? ( "\u001B[30m" + numLet ) : "  " );
+		String numLet = (char) (72 - j) + "" + (i / 6 + 1);
+		int iconRow = (whitesTurn ? 47 - i : i) % 6;
+		return (empty ? "            " : chessBoard[i / 6][j].getIcon(iconRow))
+				+ (numRow ? ("\u001B[30m" + numLet) : "  ");
 	}
 
-	public static void debug( Piece[][] chessBoard, boolean whitesTurn, int fr, int fc, int tr,	int tc)
-	{
-		String lets = whitesTurn ? "  A  B  C  D  E  F  G  H" : "  H  G  F  E  D  C  B  A";
-		System.out.println(lets);
-		int out = whitesTurn ? 7 : 0;
+	public static void debug(Piece[][] chessBoard, boolean whitesTurn, boolean useJansi, int fr, int fc, int tr,
+			int tc) {
+		String lets = whitesTurn || useJansi ? "  | A  | B  | C  | D  | E  | F  | G  | H  |"
+				: "  | H  | G  | F  | E  | D  | C  | B  | A  |";
+		String dash = "--|----|----|----|----|----|----|----|----|";
+		int out = whitesTurn || useJansi ? 7 : 0;
 		int in = out;
-		int minOut = whitesTurn ? -1 : 8;
+		int minOut = whitesTurn || useJansi ? -1 : 8;
 		int minIn = minOut;
-		int chg = whitesTurn ? -1 : 1; 
-		for ( int i = out; i != minOut; i += chg )
-		{
-			for ( int j = in; j != minIn; j += chg )
-				System.out.print(( j == 7 ? ( i + 1 ) + " " : "" ) + ( chessBoard[i][j] == null ? "nn "
-						: ( ( chessBoard[i][j].white ? "w" : "b" ) + chessBoard[i][j].toString().charAt(0) + " " ) ));
+		int chg = whitesTurn || useJansi ? -1 : 1;
+
+		System.out.println(lets);
+		for (int i = out; i != minOut; i += chg) {
+			System.out.println(dash);
+			for (int j = in; j != minIn; j += chg) {
+				System.out.print((j == out ? (i + 1) : "") + " | "
+						+ (chessBoard[i][j] == null ? "  "
+								: ((chessBoard[i][j].white ? "w" : "b") + chessBoard[i][j].toString().charAt(0)))
+						+ (j == minIn - chg ? " |" : ""));
+			}
 			System.out.println();
 		}
+		System.out.println(dash);
 	}
 
-	public static void clear() throws InterruptedException, IOException
-	{
+	public static void clear() throws InterruptedException, IOException {
 		new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
 	}
 }
