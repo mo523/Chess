795d5fa0c4560da6c3fca1a10087e6b769272a8e
mo523 <43011781+mo523@users.noreply.github.com>
2019-01-02 15:17:57 -0500

    Update Play.bat

--- Play.bat
+++ Play.bat
@@ -1,5 +1,5 @@
 @echo off
-copy jansi-1.17.1.jar src\
-javac -cp jan*.jar src\*.java
+copy jansi-1.17.1.jar src\chess\
+javac -cp jan*.jar src\chess\*.java src\exceptions\*.java
 cd src
-java -cp jansi-1.17.1.jar; ChessDriver
+java -cp jansi-1.17.1.jar; chess/ChessDriver
