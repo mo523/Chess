92893946fd8a868a2781e15b405d89fce6cebf51
mo523 <43011781+mo523@users.noreply.github.com>
2019-03-11 00:07:17 -0400

    Fixed Eclipse Warnings

--- src/chess/SaveGameFunctionality.java
+++ src/chess/SaveGameFunctionality.java
@@ -42,7 +42,6 @@ public class SaveGameFunctionality {
 		saver.close();
 	}
 
-	@SuppressWarnings("resource")
 	private static ChessBoard[] readFile(String fileName) throws IOException, ClassNotFoundException {
 		ObjectInputStream loader = null;
 		boolean found = true;
@@ -72,7 +71,6 @@ public class SaveGameFunctionality {
 		return collection;
 	}
 
-	@SuppressWarnings("resource")
 	public static ChessBoard loadSavedGame() throws FileNotFoundException, IOException, ClassNotFoundException {
 		List<ChessBoard> list = new ArrayList<ChessBoard>( loopThruFile( new ObjectInputStream(new FileInputStream("Saved Games")) ) );
 		System.out.println("Which game?");
